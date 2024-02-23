4ffd1cd31b2122c9d9ec0468be6bccdb56c5a3be
LennyTechiq <lennytechiq@gmail.com>
2024-02-23 15:54:35 +0300

    Updated the datasource url of the h2 database in the application properties file.

--- src/main/java/com/leo/jobapp/job/impl/JobServiceImpl.java
+++ src/main/java/com/leo/jobapp/job/impl/JobServiceImpl.java
@@ -5,8 +5,6 @@ import com.leo.jobapp.job.JobRepository;
 import com.leo.jobapp.job.JobService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
