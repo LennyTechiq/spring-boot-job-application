837702fe6a523dfbef4c9b021dbdab322818f3db
LennyTechiq <lennytechiq@gmail.com>
2024-03-07 16:31:38 +0300

    Implement getAllReviewsById from database.

--- src/main/java/com/leo/jobapp/review/ReviewController.java
+++ src/main/java/com/leo/jobapp/review/ReviewController.java
@@ -1,14 +1,25 @@
 package com.leo.jobapp.review;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/companies/{companyId}")
+@RequestMapping("/companies")
 public class ReviewController {
-    private ReviewService reviewService;
+    private final ReviewService reviewService;
 
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
+    }
+
+    @GetMapping("/{companyId}/reviews")
+    public ResponseEntity<List<Review>> getAllReviews(@PathVariable Long companyId) {
+        return new ResponseEntity<>(reviewService.getAllReviews(companyId), HttpStatus.OK);
     }
 }
--- src/main/java/com/leo/jobapp/review/impl/ReviewServiceImpl.java
+++ src/main/java/com/leo/jobapp/review/impl/ReviewServiceImpl.java
@@ -1,8 +1,22 @@
 package com.leo.jobapp.review.impl;
 
+import com.leo.jobapp.review.Review;
+import com.leo.jobapp.review.ReviewRepository;
 import com.leo.jobapp.review.ReviewService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReviewServiceImpl implements ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public List<Review> getAllReviews(Long companyId) {
+        return reviewRepository.findByCompanyId(companyId);
+    }
 }
