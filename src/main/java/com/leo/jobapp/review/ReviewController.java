08d12b110038a7bb532cf8eddadb5778d43a6989
LennyTechiq <lennytechiq@gmail.com>
2024-03-07 16:51:29 +0300

    Implement post review.

--- src/main/java/com/leo/jobapp/review/ReviewController.java
+++ src/main/java/com/leo/jobapp/review/ReviewController.java
@@ -2,15 +2,12 @@ package com.leo.jobapp.review;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/companies")
+@RequestMapping("/companies/{companyId}")
 public class ReviewController {
     private final ReviewService reviewService;
 
@@ -18,8 +15,18 @@ public class ReviewController {
         this.reviewService = reviewService;
     }
 
-    @GetMapping("/{companyId}/reviews")
+    @GetMapping("/reviews")
     public ResponseEntity<List<Review>> getAllReviews(@PathVariable Long companyId) {
         return new ResponseEntity<>(reviewService.getAllReviews(companyId), HttpStatus.OK);
+    }
+
+    @PostMapping("/reviews")
+    public ResponseEntity<String> addReview(@PathVariable Long companyId, @RequestBody Review review) {
+        boolean isReviewSaved = reviewService.addReview(companyId, review);
+        if (isReviewSaved)
+            return new ResponseEntity<>("Review added successful!!", HttpStatus.CREATED);
+        else
+            return new ResponseEntity<>("Review Not Saved!!!", HttpStatus.NOT_FOUND);
+
     }
 }
--- src/main/java/com/leo/jobapp/review/impl/ReviewServiceImpl.java
+++ src/main/java/com/leo/jobapp/review/impl/ReviewServiceImpl.java
@@ -1,5 +1,7 @@
 package com.leo.jobapp.review.impl;
 
+import com.leo.jobapp.company.Company;
+import com.leo.jobapp.company.CompanyService;
 import com.leo.jobapp.review.Review;
 import com.leo.jobapp.review.ReviewRepository;
 import com.leo.jobapp.review.ReviewService;
@@ -10,13 +12,27 @@ import java.util.List;
 @Service
 public class ReviewServiceImpl implements ReviewService {
     private final ReviewRepository reviewRepository;
+    private CompanyService companyService;
 
-    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+    public ReviewServiceImpl(ReviewRepository reviewRepository, CompanyService companyService) {
         this.reviewRepository = reviewRepository;
+        this.companyService = companyService;
     }
 
     @Override
     public List<Review> getAllReviews(Long companyId) {
         return reviewRepository.findByCompanyId(companyId);
+    }
+
+    @Override
+    public boolean addReview(Long companyId, Review review) {
+        Company company = companyService.getCompanyById(companyId);
+        if(company != null) {
+            review.setCompany(company);
+            reviewRepository.save(review);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
